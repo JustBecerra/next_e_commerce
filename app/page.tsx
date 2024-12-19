e0ca75f2b1c411996accc7a9dc30d370d1b9d6c8
JustBecerra <justj.becerra@gmail.com>
2024-12-19 11:56:32 -0300

    add card component

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
-import { LoginCard } from "@/components/logincard"
+import LoginCard from "@/components/logincard"
 
 export default function Login() {
 	return (
-		<div className="min-h-screen">
+		<div className="min-h-screen flex justify-center items-center">
 			<LoginCard />
 		</div>
 	)
